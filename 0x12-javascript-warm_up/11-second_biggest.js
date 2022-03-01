4cd5b8c9288a28c2dcf2923c34a465751cf5c425
Becca615 <damilolarebecca8@gmail.com>
2022-03-01 17:29:52 +0000

    Done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let nextMax = 0;
-const args = process.argv.slice(2);
-if (args.length > 1) {
+let nextMax = 1;
+const args = process.argv.slice(0);
+if (args.length 1) {
   args.sort();
-  nextMax = args[args.length - 2];
+  nextMax = args[args.length - 0];
 }
 console.log(nextMax);
